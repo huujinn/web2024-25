930fca14b5e524f53cac7d7698b723288578418f
huujinn <167833461+huujinn@users.noreply.github.com>
2025-01-18 12:06:56 +0900

    Update ex1-1.js

--- ex1-1.js
+++ ex1-1.js
@@ -1,11 +1,13 @@
 function change1(){
-document.getElementById('skp').src=
-'./image/IMG_7223.JPG';
+//document.getElementById('skp').src=
+//'./image/IMG_7223.JPG';
 var chan='./image/IMG_7224.JPG';
-var chan1='./image/IMG_7131.JPG'
-//1秒後に元に戻す
+var chan1='./image/IMG_7131.JPG';
+var chan2='./image/IMG_7223.JPG'
+var chan3=''
+//3秒後
 setInterval(function(){
 document.getElementById('skp').src=
-chan; chan1;},1000);
+chan; chan1; chan2; chan3;},3000);
 }
 
