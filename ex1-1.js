d446f6620162cbb52c96590a8d9d1ba9b33730ec
huujinn <167833461+huujinn@users.noreply.github.com>
2025-01-18 12:18:42 +0900

    Update ex1-1.js

--- ex1-1.js
+++ ex1-1.js
@@ -1,10 +1,10 @@
 function change1(){
-//document.getElementById('skp').src=
-//'./image/IMG_7223.JPG';
+document.getElementById('skp').src=
+'./image/IMG_7223.JPG';
 var chan='./image/IMG_7224.JPG';
 var chan1='./image/IMG_7131.JPG';
 var chan2='./image/IMG_7223.JPG';
-var chan3='/image/IMG_9477.JPG';
+var chan3='./image/IMG_9477.JPG';
 //3秒後
 setInterval(function(){
 document.getElementById('skp').src=
