9ec22e1194a6d844c91596d9718fcaf27411802c
huujinn <167833461+huujinn@users.noreply.github.com>
2025-01-18 14:03:17 +0900

    Update ex1-2.js

--- ex1-2.js
+++ ex1-2.js
@@ -1,9 +1,10 @@
 function change1(){
 document.getElementById('sp').src=
-'./image/IMG_7223.JPG';
+'./image/IMG_7224.JPG';
 var chan='./image/IMG_7131.JPG';
 //3秒後
 setTimeout(function(){
 document.getElementById('sp').src=
 chan;},3000);
 }
+
