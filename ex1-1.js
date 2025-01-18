7acbdb58cb2f3b83cce586cfe6eb83d3d61f5d01
huujinn <167833461+huujinn@users.noreply.github.com>
2025-01-18 11:41:22 +0900

    Update ex1-1.js

--- ex1-1.js
+++ ex1-1.js
@@ -1,1 +1,10 @@
+function change1(){
+document.getElementById('skp').src=
+'./image/OIP.jpg';
+var chan='./image/bulb-off.png';
+//1秒後に元に戻す
+setInterval(function(){
+document.getElementById('skp').src=
+chan;},1000);
+}
 
